7fc8e72b8043d7ae6f44e076d95c3464d2e1d70f
Arshdeep724 <singharshdeep46@gmail.com>
2024-03-07 17:55:55 +0530

    perf: :zap: added Hot module replacement

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,18 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
 
+declare const module: any;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3000;
   app.enableCors();
   await app.listen(port);
   Logger.log(`🚀 Application is running on: http://localhost:${port}`);
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 bootstrap();
